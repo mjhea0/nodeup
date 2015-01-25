1a0582e600940476ff8f08f50e55f37ea332ebc1
Michael Herman <hermanmu@gmail.com>
2015-01-25 10:39:42 -0700

    updated app.js

---
+++ routes/index.js
@@ -0,0 +1,39 @@
+var express = require('express'),
+    router = express.Router(),
+    passport = require('passport');
+
+
+router.get('/', function(req, res) {
+    res.render('index', { user: req.user });
+});
+
+router.get('/account', ensureAuthenticated, function(req, res){
+    res.render('account', { user: req.user });
+});
+
+router.get('/login', function(req, res){
+    res.render('login', { user: req.user });
+});
+
+router.get('/auth/github',
+    passport.authenticate('github'),
+    function(req, res){});
+
+router.get('/auth/github/callback',
+    passport.authenticate('github', { failureRedirect: '/login' }),
+    function(req, res) {
+        res.redirect('/');
+});
+
+router.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
+
+function ensureAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) { return next(); }
+    res.redirect('/login');
+}
+
+
+module.exports = router;
