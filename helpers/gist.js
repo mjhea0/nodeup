ff8f78cc90046252839c5051954b1603a0a1b751
Michael Herman <hermanmu@gmail.com>
2015-01-26 17:03:02 -0700

    added github api call helper

---
+++ helpers/gist.js
@@ -0,0 +1,24 @@
+var request = require('request');
+
+var postData = {
+    description: 'test',
+    public: false,
+    files: {"test.txt":{"content":"String file contents"}}
+};
+var url = 'https://api.github.com/gists';
+var options = {
+  method: 'post',
+  body: postData,
+  json: true,
+  url: url
+};
+
+request(options, function (err, res, body) {
+  if (err) {
+    inspect(err, 'error posting json');
+    return;
+  }
+  var headers = res.headers;
+  var statusCode = res.statusCode;
+  console.log(body);
+});
