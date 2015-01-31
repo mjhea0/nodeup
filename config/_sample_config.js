c5d693afdfb5fd5d02c594a17d9e05b0538a5ccd
mjhea0 <hermanmu@gmail.com>
2015-01-31 09:37:05 -0700

    sample config

--- config/_sample_config.js
+++ config/_sample_config.js
@@ -2,6 +2,6 @@ var config = {};
 
 config.github_client_id = process.env.github_client_id || 'GET_YOUR_OWN';
 config.github_client_secret = process.env.github_client_secret || 'GET_YOUR_OWN';
-config.github_callback_url = process.env.github_callback_url || "http://127.0.0.1:1337/auth/github/callback";
+config.github_callback_url = process.env.github_callback_url || "http://127.0.0.1:3000/auth/github/callback";
 
 module.exports = config;
