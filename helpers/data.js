535117393124c25aaa5557ded892fda80b2fbfd1
mjhea0 <hermanmu@gmail.com>
2015-02-01 18:17:35 -0700

    added helper to add dummy exercises

--- helpers/data.js
+++ helpers/data.js
@@ -1,23 +1,33 @@
-// helpers for creating database database data
+// helpers for creating database data
 
 var Exercise = require('../models/exercises.js');
 
 var createExercises = function() {
 
-  exerciseOne = new Exercise({
-    name: "fizz buzz fizz",
-    difficulty: "hard",
-    created: Date.now(),
-    url: "N/A"
-  });
-  exerciseOne.save(function(err, exerciseOne) {
-      if(err) {
-        console.log(err);
-      } else {
-        console.log("saving exercise...");
+  var nameOne = "fizz buzz fizz";
+
+  Exercise.findOne({ name: nameOne }, function(err, exercise) {
+      if(err) { console.log(err); }
+      if (!err && exercise === null) {
+        exerciseOne = new Exercise({
+          name: nameOne,
+          difficulty: "hard",
+          created: Date.now(),
+          url: "N/A"
+        });
+        exerciseOne.save(function(err, exerciseOne) {
+            if(err) {
+              console.log(err);
+            } else {
+              console.log("saving exercise...");
+            }
+        });
       }
   });
 
 };
 
 module.exports = createExercises;
+
+
+
